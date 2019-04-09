7208db81554fa56edf79678cc5337ceee11051a5
“pakeecbala” <pakeecbala@hotmail.com>
2019-04-09 11:03:24 +0200

    added grid

---
+++ src/components/PropertyDetail/PropertyDetail.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PropertyDetail extends React.Component {
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+ReactDOM.render(<PropertyDetail />, document.getElementById("case-frontend"));

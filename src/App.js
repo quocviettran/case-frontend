68e0e03129d6e33fc2a34d090c229b16fc4e7e31
“pakeecbala” <pakeecbala@hotmail.com>
2019-04-09 14:57:24 +0200

    added property details

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Register from "./container/Register/Register";
 import logo from "./logo.svg";
 import Properties from "./components/Properties/Properties";
 import PropertiesCard from "./components/PropertiesCard/PropertiesCard";
+import PropertieDetail from "./components/PropertyDetail/PropertyDetail";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PropertiesCard />
+        <PropertieDetail />
       </div>
     );
   }
--- src/components/PropertyDetail/PropertyDetail.js
+++ src/components/PropertyDetail/PropertyDetail.js
@@ -1,9 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
+import { Form, Grid, Header, Image, Message, Segment } from "semantic-ui-react";
 
-class PropertyDetail extends React.Component {
-  render() {
-    return <div>Test</div>;
-  }
-}
+const propertyDetail = () => (
+  <div className="login-form">
+    {/*
+      Heads up! The styles below are necessary for the correct render of this example.
+      You can do same with CSS, the main idea is that all the elements up to the `Grid`
+      below must have a height of 100%.
+    */}
+    <style>
+      {`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}
+    </style>
+    <Grid textAlign="center" style={{ height: "100%" }} verticalAlign="top">
+      <Grid.Column style={{ maxWidth: 1000 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          <Image src="/logo.png" />
+        </Header>
+        <Segment streched>
+          <h1>HEI HEI</h1>
+        </Segment>
+        <Segment streched>
+          <h1>Hahahah</h1>
+        </Segment>
+        <p>hahahaha</p>
+      </Grid.Column>
+    </Grid>
+  </div>
+);
 
-ReactDOM.render(<PropertyDetail />, document.getElementById("case-frontend"));
+export default propertyDetail;

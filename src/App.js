36e0f95b0ce56954ccd3b2ac6fc174891c01cdd7
“pakeecbala” <pakeecbala@hotmail.com>
2019-04-09 13:18:03 +0200

    added PropertiesCard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import Register from './container/Register/Register'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Register from "./container/Register/Register";
+import logo from "./logo.svg";
+import Properties from "./components/Properties/Properties";
+import PropertiesCard from "./components/PropertiesCard/PropertiesCard";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Register/>
+        <PropertiesCard />
       </div>
     );
   }

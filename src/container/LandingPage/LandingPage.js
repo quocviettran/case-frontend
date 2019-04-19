33743b3be90b864d17c95a65407f6e39a9d14c98
Michael <Michael.Dejene.Kifle@no.experis.com>
2019-04-19 11:48:58 +0200

    merge fix

--- src/container/LandingPage/LandingPage.js
+++ src/container/LandingPage/LandingPage.js
@@ -13,7 +13,6 @@ class LandingPage extends Component{
                         <Login/>            
                     </Grid.Column>
                     <Grid.Column width={8}>
-                        <Register/>
                     </Grid.Column>
                 </Grid>
             </Container>
